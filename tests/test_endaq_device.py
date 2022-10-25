174eadca1eda345f568308ae67fda6ca66f8a00c
David Stokes <dstokes@mide.com>
2022-10-25 14:11:06 -0400

    Fixed fake recorder scraping

--- tests/test_endaq_device.py
+++ tests/test_endaq_device.py
@@ -6,7 +6,8 @@ from endaq.device import getRecorder
 
 RECORDERS_ROOT = os.path.join(os.path.dirname(__file__), 'fake_recorders')
 RECORDERS = [os.path.join(RECORDERS_ROOT, d) for d in os.listdir(RECORDERS_ROOT)
-             if not d.startswith(('.', '_'))]
+             if os.path.isdir(os.path.join(RECORDERS_ROOT, d))
+             and not d.startswith(('.', '_'))]
 
 
 @pytest.mark.parametrize("path", RECORDERS)
