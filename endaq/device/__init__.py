f43b81a6a3d1629dc579f21b7b4829a067b7991c
David Stokes <dstokes@mide.com>
2023-04-21 13:01:32 -0400

    Version bump (1.0.5)

--- endaq/device/__init__.py
+++ endaq/device/__init__.py
@@ -21,7 +21,7 @@ from .types import Filename, Epoch
 
 from . import schemata
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 __all__ = ('CommandError', 'ConfigError', 'ConfigVersionError',
            'DeviceError', 'DeviceTimeout', 'UnsupportedFeature',
