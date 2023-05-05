5d2f86effea0a8dd4b581a83d601392fa0babaea
David Stokes <dstokes@mide.com>
2023-05-05 14:18:43 -0400

    Created mock serial port, separated FileCommandInterface I/O

--- tests/mock_hardware.py
+++ tests/mock_hardware.py
@@ -8,10 +8,123 @@ from endaq.device.command_interfaces import CommandInterface, FileCommandInterfa
 from endaq.device.hdlc import hdlc_encode
 
 
-class MockCommandIO:
-    """ Replacement for a `CommandInterface`'s low-level command-writing and
-        response-reading. It allows testing without hardware, the command
-        packet can be inspected, and an arbitrary responce can be specified.
+class MockPort:
+    """
+    A fake serial port. It collects data written to it, and reading it
+    returns a canned response.
+
+    Note: This only implements a subset of `serial.Serial` methods. More may
+    need to be added if changes are made to `SerialCommandInterface`.
+    """
+
+    def __init__(self, response=b'', chunksize=None):
+        """ A fake serial port. Arguments are also attributes which
+            can be set at any time.
+
+            :param response: Data to be returned on `read()`.
+            :param chunksize: The number of bytes returned each read, to
+                simulate data arriving during the `read()`.
+        """
+        self.is_open = False
+        self.timeout = 0
+
+        self.input = b''
+        self.response = response
+        self.chunkSize = chunksize
+
+    def open(self):
+        self.is_open = True
+
+    def close(self):
+        self.is_open = False
+
+    def read(self, size=None):
+        size = size or len(self.response)
+        if self.chunkSize:
+            size = min(size, self.chunkSize)
+
+        result = self.response[:size]
+        self.data = self.response[size:]
+        return result
+
+    def write(self, data):
+        self.input += data
+
+    def flushInput(self):
+        pass
+
+    def flushOutput(self):
+        pass
+
+    def flush(self):
+        pass
+
+    @property
+    def in_waiting(self):
+        return len(self.response)
+
+
+class MockCommandFileIO:
+    """ Replaces the low-level communication in a device's `FileCommandInterface`.
+        It allows testing without hardware, the command packet can be inspected,
+        and an arbitrary response can be generated.
+
+        Specifically, `MockCommandFileIO` replaces the COMMAND file writing
+        and RESPONSE file reading methods of the device's `FileCommandInterface`
+        instance. It also provides a method for encoding responses as returned
+        via the serial command interface.
+    """
+
+    def __init__(self, device: endaq.device.Recorder):
+        """ Replacement for a device's `FileCommandInterface`.
+
+            :param device: The device under test.
+        """
+        self.device = device
+        device.command._writeCommand = self._writeCommand
+        device.command._readResponse = self._readResponse
+
+        self.response = None  # The raw data to send in response
+
+
+    def _writeCommand(self, packet):
+        """ A replacement for the `_writeCommand()` of the `CommandInterface`
+            under test. It just stores the raw packet for inspection.
+        """
+        self.command = packet
+        return len(packet)
+
+
+    def _readResponse(self, timeout=None, callback=None):
+        """ A replacement for the `_readResponse()` of the `CommandInterface`
+            under test. It stores the calling arguments for inspection and
+            returns `self.response`.
+        """
+        self.responseArgs = (timeout, callback)
+        return self.response
+
+
+    def encodeResponse(self, response: dict) -> bytearray:
+        """ Utility method to encode a dictionary containing a device
+            response into the raw format expected of the device.
+
+            :param response: The response dictionary.
+            :return: The response as raw binary.
+        """
+        return CommandInterface._encode(self.device.command, response)
+
+
+
+class MockCommandSerialIO:
+    """ Replaces the low-level communication in a device's
+        `SerialCommandInterface`. It allows testing without hardware, the
+        command packet can be inspected, and an arbitrary response can be
+        specified.
+
+        Specifically, `MockCommandSerialIO` replaces the device's
+        `SerialCommandInterface`serial port instance with an instance of
+        `MockPort`, and provides a utility method for encoding responses as
+        returned via the serial command interface.
 
         Usage (`dev` is an instance of an `endaq.device.Recorder` subclass):
 
@@ -26,25 +139,30 @@ class MockCommandIO:
 
     """
 
-
     def __init__(self, device: endaq.device.Recorder):
-        """ Replacement for a device's `CommandInterface`.
+        """ Replacement for a device's `SerialCommandInterface`.
 
             :param device: The device under test.
         """
         self.device = device
-        self.command = None  # The last command sent by the device
-        self.response = None  # The raw data to send in response
-        self.responseArgs = None
+        self.port = MockPort()
+        device.command.port = self.port
+        device.command.getSerialPort = self.getSerialPort
 
-        device.command._writeCommand = self.writeCommand
-        device.command._readResponse = self.readResponse
-        device._getSerialPort = self.getSerialPort
+
+    @property
+    def response(self):
+        return self.port.response
+
+
+    @response.setter
+    def response(self, data):
+        self.port.response = data
 
 
     def encodeResponse(self, response: dict, resultcode: int = 0) -> bytearray:
-        """ Encode a dictionary containing a device response into the raw
-            format expected of the device.
+        """ Utility method to encode a dictionary containing a device
+            response into the raw format expected from the device.
 
             :param response: The response dictionary.
             :param resultcode: The Corbus response code. Not applicable to
@@ -52,33 +170,13 @@ class MockCommandIO:
             :return: The response as raw binary.
         """
         response = CommandInterface._encode(self.device.command, response)
-        if isinstance(self.device.command, FileCommandInterface):
-            return response
-
-        response = bytearray([0x81, 0x00, resultcode]) + response
-        return hdlc_encode(response)
+        return hdlc_encode(bytearray([0x81, 0x00, resultcode]) + response)
 
 
     def getSerialPort(self, *args, **kwargs):
         """ A replacement for the `_getSerialPort()` of the `CommandInterface`
-            under test. It does nothing.
+            under test. It does nothing but return the object's `MockPort`
+            instance.
         """
-        return None
-
-
-    def writeCommand(self, packet):
-        """ A replacement for the `_writeCommand()` of the `CommandInterface`
-            under test. It just stores the raw packet for inspection.
-        """
-        self.command = packet
-        return len(packet)
-
-
-    def readResponse(self, timeout=None, callback=None):
-        """ A replacement for the `_readResponse()` of the `CommandInterface`
-            under test. It stores the calling arguments for inspection and
-            returns `self.response`.
-        """
-        self.responseArgs = (timeout, callback)
-        return self.response
+        return self.port
 
--- tests/test_command.py
+++ tests/test_command.py
@@ -7,9 +7,10 @@ import pytest
 
 import endaq.device
 from endaq.device import getRecorder
-from endaq.device import command_interfaces
+from endaq.device.command_interfaces import CommandInterface, FileCommandInterface
 
 from .fake_recorders import RECORDER_PATHS
+from .mock_hardware import MockCommandSerialIO
 
 # Clear any cached devices, just to be safe
 endaq.device.RECORDERS.clear()
@@ -17,10 +18,27 @@ endaq.device.RECORDERS.clear()
 # Create parameters, mainly to provide an ID, making the results readable
 DEVICES = [pytest.param(getRecorder(path, strict=False), id=os.path.basename(path)) for path in RECORDER_PATHS]
 
+SERIAL_DEVICES = [param for param in DEVICES if not isinstance(param[0][0].command, FileCommandInterface)]
+
 
 @pytest.mark.parametrize("dev", DEVICES)
 def test_command_basics(dev):
     """ Initial 'sanity test' to verify `CommandInterface` instances are
         being instantiated.
     """
-    assert isinstance(dev.command, command_interfaces.CommandInterface)
+    assert isinstance(dev.command, CommandInterface)
+
+
+@pytest.mark.parametrize("dev", SERIAL_DEVICES)
+def test_command_ping(dev):
+    """ Test the `ping()` command on devices that support it.
+    """
+    mock_io = MockCommandSerialIO(dev)
+    mock_io.response = mock_io.encodeResponse({'EBMLResponse':
+                                               {'ResponseIdx': 1,
+                                                'CMDQueueDepth': 1,
+                                                'DeviceStatusCode': 0,
+                                                'PingReply': bytearray(b'hello')}},
+                                              resultcode=0)
+    
+    assert dev.command.ping() == bytearray(b'hello')
