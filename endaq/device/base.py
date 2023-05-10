7a11282e354f43dccc29d93edbd86773bd57ce1a
David Stokes <dstokes@mide.com>
2023-05-10 15:23:15 -0400

    Made attempting to read manifest file come before USERPG* files

--- endaq/device/base.py
+++ endaq/device/base.py
@@ -951,10 +951,10 @@ class Recorder:
             if self.isVirtual or self._manifest is not None:
                 return self._manifest
 
-            if os.path.exists(os.path.join(self.path, self._USERPAGE_FILE % 0)):
-                self._readUserpage()
-            elif os.path.exists(os.path.join(self.path, self._MANIFEST_FILE)):
+            if os.path.exists(os.path.join(self.path, self._MANIFEST_FILE)):
                 self._readManifest()
+            elif os.path.exists(os.path.join(self.path, self._USERPAGE_FILE % 0)):
+                self._readUserpage()
 
             return self._manifest
 
