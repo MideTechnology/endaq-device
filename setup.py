62ea3d9a07575ef4cace9fd1b9c9ab0f3c129119
David Stokes <dstokes@mide.com>
2023-11-07 16:03:09 -0500

    Added URLs to setup, added Python 3.11 to unit test workflow

--- setup.py
+++ setup.py
@@ -61,8 +61,14 @@ setuptools.setup(
                      'Programming Language :: Python :: 3.8',
                      'Programming Language :: Python :: 3.9',
                      'Programming Language :: Python :: 3.10',
+                     'Programming Language :: Python :: 3.11',
                      ],
         keywords='endaq configure recorder hardware',
+        project_urls={
+            "Bug Tracker": "https://github.com/MideTechnology/endaq-device/issues",
+            "Documentation": "https://mide-technology-endaq-device.readthedocs-hosted.com/en/latest/",
+            "Source Code": "https://github.com/MideTechnology/endaq-device",
+            },
         packages=[
             'endaq.device',
             'endaq.device.ui_defaults',
