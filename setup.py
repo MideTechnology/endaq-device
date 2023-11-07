b7fa81222212eb6904d89ef4b2bc456d80399dab
David Stokes <dstokes@mide.com>
2023-11-07 16:13:21 -0500

    Added Python 3.12 to unit tests

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setuptools.setup(
                      'Programming Language :: Python :: 3.9',
                      'Programming Language :: Python :: 3.10',
                      'Programming Language :: Python :: 3.11',
+                     'Programming Language :: Python :: 3.12',
                      ],
         keywords='endaq configure recorder hardware',
         project_urls={
