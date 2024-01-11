96ac6a602e5e1581b1cc8f2b238029c4ff808276
David Stokes <dstokes@mide.com>
2024-01-11 11:11:19 -0500

    Removed `getRecorder()` type check (more trouble than worth)

--- endaq/device/__init__.py
+++ endaq/device/__init__.py
@@ -87,9 +87,6 @@ def getRecorder(path: Filename,
     """
     global RECORDERS
 
-    if not isinstance(path, (str, Path, Drive)):
-        raise TypeError('Invalid type for path: {}'.format(type(path).__name__))
-
     dev = None
 
     for rtype in RECORDER_TYPES:
