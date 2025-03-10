193f306a4b02f42f43a257cb26c7ffd173adabaa
stokesMIDE <dstokes@mide.com>
2025-03-10 13:32:56 -0400

    Restored `levenshtein()`

--- endaq/device/util.py
+++ endaq/device/util.py
@@ -78,3 +78,26 @@ def dump(data: ByteString, length: int = 8) -> str:
     if not length:
         length = len(data)
     return ' '.join(f'{x:02x}' for x in data[:length])
+
+
+def levenshtein(a: str, b: str) -> int:
+    """Calculates the Levenshtein distance between a and b.
+    """
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a, b = b, a
+        n, m = m, n
+
+    current = range(n + 1)
+    for i in range(1, m + 1):
+        previous, current = current, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete = previous[j] + 1, current[j - 1] + 1
+            change = previous[j - 1]
+            if a[j - 1] != b[i - 1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+
+    return current[n]
+
