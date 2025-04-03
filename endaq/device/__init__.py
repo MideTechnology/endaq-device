98db3594e84c4009e40b16c757585a2247f381c3
stokesMIDE <dstokes@mide.com>
2025-04-03 10:24:13 -0400

    Version bump (1.4.1b4)

--- endaq/device/__init__.py
+++ endaq/device/__init__.py
@@ -33,7 +33,7 @@ from .types import Drive, Filename, Epoch
 #
 # ============================================================================
 
-__version__ = "1.4.1b3"
+__version__ = "1.4.1b4"
 
 __all__ = ('CommandError', 'ConfigError', 'ConfigVersionError',
            'DeviceError', 'DeviceTimeout', 'UnsupportedFeature',
