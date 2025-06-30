44f367d3f5b266810627d37b9d031201128f1014
Elin O'Neill <eoneill@hutchinsoninc.com>
2025-06-30 10:41:15 -0400

    Added 'device-needed' pytest marker.

--- test_hardware/conftest.py
+++ test_hardware/conftest.py
@@ -26,6 +26,9 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers", "device_s: mark test to run only when an S is connected"
     )
+    config.addinivalue_line(
+        "markers", "device_needed: mark test to run only when a device is connected"
+    )
 
 
 def pytest_collection_modifyitems(config, items):
@@ -34,7 +37,11 @@ def pytest_collection_modifyitems(config, items):
     command line input.
     """
     if config.getoption("--device") is None:
-        pass
+        skip_test = pytest.mark.skip(
+            reason="Run using local option, skipping device required tests")
+        for item in items:
+            if "device_needed" in item.keywords:
+                item.add_marker(skip_test)
     elif config.getoption("--device")[0] == "S":
         skip_test = pytest.mark.skip(
             reason="Run using local option, skipping device required tests")
