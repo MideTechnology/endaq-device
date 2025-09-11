ea3e2d1fff2fece1b950d8f6559821b1805969f7
Peter Scheidler <pscheidler@gmail.com>
2025-09-11 13:47:58 -0400

    Tested with psutil 7.0.0 and 6.0.0

--- endaq/device/linux.py
+++ endaq/device/linux.py
@@ -155,32 +155,26 @@ def readRecorderClock(clockFile: Filename,
 def getDeviceList(types: dict, strict: bool = True) -> list:
     """ Get a list of data recorders, as their respective mount points.
     """
-    print(f"Running getDeviceList 1")
     result = set()
     found_devices = set()       # Working around a linux specific issue where sometimes multiple device dirs point to
                                 # the same mount point
     for device in psutil.disk_partitions():
-        print(f"Checking device {device}")
         try:
-            print(f"checking existance")
             if not os.path.exists(device.mountpoint):
                 continue
             # Skip devices we've already found (Linux oddity workaround)
             if device.mountpoint in found_devices:
                 continue
             for t in types:
-                print(f"checking type {t}")
                 if t.isRecorder(device.mountpoint, strict=strict):
                     result.add(device.mountpoint)
                     found_devices.add(device.mountpoint)
         except IOError as err:
-            print(f"IOError: {err}")
             # Rare error, may be caused by flaky device or USB.
             msg = (f"getDeviceList(): Could not access {device[0]} ({err}); "
                    "ignoring error and continuing")
             warnings.warn(msg)
             logger.error(msg)
-    print(f"Result: {result}")
     return sorted(result)
 
 
