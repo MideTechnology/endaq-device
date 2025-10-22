ab049135c618640510543968eac76496b5436dbd
Peter Scheidler <pscheidler@gmail.com>
2025-10-22 12:43:09 -0400

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -84,14 +84,7 @@ setuptools.setup(
             '': ['schemata/*.xml'],
         },
         test_suite='tests',
-        install_requires=[
-            'idelib>=3.2',
-            'numpy>=1.19.4',
-            'ebmlite>=3.1.0',
-            'psutil>=5.5.0, <6.0.0; sys_platform == "linux" or sys_platform=="darwin"',
-            'pyserial>=3.5',
-            'pywin32>=228; sys_platform == "win32"'
-            ],
+        install_requires=INSTALL_REQUIRES,
         extras_require={
             'test': INSTALL_REQUIRES + TEST_REQUIRES,
             'docs': INSTALL_REQUIRES + DOCS_REQUIRES,
