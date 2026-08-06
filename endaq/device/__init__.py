7e0abede472370f9616ce03abecd963bfae248ae
stokesMIDE <dstokes@mide.com>
2026-08-06 16:33:38 -0400

    beta version bump

--- endaq/device/__init__.py
+++ endaq/device/__init__.py
@@ -37,7 +37,7 @@ from .types import Drive, Filename, Epoch
 #
 # ============================================================================
 
-__version__ = "1.4.1b6"
+__version__ = "1.4.1b7"
 
 __all__ = ('CommandError', 'ConfigError', 'ConfigVersionError',
            'DeviceError', 'DeviceTimeout', 'UnsupportedFeature',
